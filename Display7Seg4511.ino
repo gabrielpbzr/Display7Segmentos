5b3b1a326a272cb56474d465a9806cc156d7974f
Gabriel P. Bezerra <gabrielpbzr@hotmail.com>
2017-12-27 21:57:02 -0200

    Primeiro Commit

---
+++ Display7Seg4511.ino
@@ -0,0 +1,48 @@
+/*
+ * Controle de dois displays de sete segmentos catodo comum
+ * usando o CI 4511 multiplexado
+ * Author: Gabriel P. Bezerra <gabrielpbzr@gmail.com>
+ * Date: 2017-12-27
+ */
+const byte DISPLAY_DELAY = 10;
+const int INTERVAL = 1000;
+byte count = 0;
+byte rem = 0;
+unsigned long lastRead = 0;
+unsigned long delta = 0;
+
+void writeValue(byte value) {
+  // Escreve o valor no primeiro display
+  PORTB = value / 10; 
+  PORTC = 1;
+  delay(DISPLAY_DELAY);
+  // Habilita e escreve no segundo display
+  PORTB = value % 10;
+  PORTC = 2;
+  delay(DISPLAY_DELAY);
+}
+
+void setup() {
+  DDRB = 0b00001111;
+  DDRC = 0b00000011;
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  unsigned long now = millis();
+  delta = now - lastRead;    
+  if (delta >= INTERVAL) {
+    lastRead = now;
+    count++;
+    count = count % 60;
+  }
+  writeValue(count);
+}
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# Contador 7 Segmentos
+
+Projeto de um contador utilizando o ATMEGA328P com o driver CD4511 e 
+dois displays de 7 segmentos.
+
