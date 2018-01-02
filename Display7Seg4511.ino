86ce34e5c5dace0b9a61f477ec091b5d5f64313b
Gabriel P. Bezerra <gabrielpbzr@gmail.com>
2018-01-02 09:25:00 -0200

    Melhorias na documentação do código

--- Display7Seg4511.ino
+++ Display7Seg4511.ino
@@ -4,34 +4,46 @@
  * Author: Gabriel P. Bezerra <gabrielpbzr@gmail.com>
  * Date: 2017-12-27
  */
-const byte DISPLAY_DELAY = 10;
-const int INTERVAL = 1000;
+const byte DISPLAY_DELAY_MS = 10;
+const int INTERVAL_MS = 1000;
 byte count = 0;
 byte rem = 0;
 unsigned long lastRead = 0;
 unsigned long delta = 0;
 
 void writeValue(byte value) {
-  // Escreve o valor no primeiro display
+  // Escreve o valor das dezenas no primeiro display
   PORTB = value / 10; 
+  // Habilita o display 1
   PORTC = 1;
-  delay(DISPLAY_DELAY);
-  // Habilita e escreve no segundo display
+  // Aguarda o tempo para mostrar o digito
+  delay(DISPLAY_DELAY_MS);
+  
+  // Escreve o valor das unidades no segundo display
   PORTB = value % 10;
+  // Habilita o display 2
   PORTC = 2;
-  delay(DISPLAY_DELAY);
+  // Aguarda o tempo para mostrar o digito
+  delay(DISPLAY_DELAY_MS);
 }
 
 void setup() {
+  // Configura o registrador PORTB
   DDRB = 0b00001111;
+  // Configura o registrador PORTC
   DDRC = 0b00000011;
 }
 
 
 void loop() {
   unsigned long now = millis();
-  delta = now - lastRead;    
-  if (delta >= INTERVAL) {
+  // Calcula a diferenca no tempo da ultima leitura
+  delta = now - lastRead;
+  /*
+   * Se a diferenca for igual ou maior o intervalo especificado, 
+   * atualize o contador
+   */
+  if (delta >= INTERVAL_MS) {
     lastRead = now;
     count++;
     count = count % 60;
